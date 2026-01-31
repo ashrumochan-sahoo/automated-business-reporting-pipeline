97043fdb2f6305542ca9546acd928ed807d909f8
Ashrumochan Sahoo <47909586+ashrumochan-sahoo@users.noreply.github.com>
2026-01-31 19:56:23 +0530

    Add core dimension and fact tables for sales analytics

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,3 +6,31 @@ CREATE TABLE dim_customer (
     created_date DATE
 );
 
+
+CREATE TABLE dim_product (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    unit_price DECIMAL(10,2)
+)
+
+
+CREATE TABLE dim_date (
+    date_id INT PRIMARY KEY,      -- Format: YYYYMMDD
+    full_date DATE,
+    year INT,
+    month INT,
+    day INT,
+    weekday VARCHAR(10)
+);
+
+
+CREATE TABLE fact_sales (
+    sales_id INT PRIMARY KEY,
+    date_id INT,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    total_amount DECIMAL(12,2)
+);
+
