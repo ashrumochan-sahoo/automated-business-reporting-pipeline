43f8e239e0e7f8db6f15d3b08efe8b57a01fd4ee
Ashrumochan Sahoo <47909586+ashrumochan-sahoo@users.noreply.github.com>
2026-02-01 22:25:04 +0530

    Refactor pipeline to handle multiple tables

--- pipeline/main.py
+++ pipeline/main.py
@@ -6,11 +6,11 @@ from pipeline.data_quality import validate_sales_data
 
 def run_pipeline():
     raw_df = extract_sales_data("data/raw/sales_data.csv")
-    transformed_df = transform_sales_data(raw_df)
+    tables = transform_sales_data(raw_df)
 
-    validate_sales_data(transformed_df)
+    validate_sales_data(tables["fact_sales"])
 
-    load_sales_data(transformed_df)
+    load_sales_data(tables)
 
 
 if __name__ == "__main__":
